72fd5e927ba0f6fde444b45cbfa28c440b1a816f
Pablo Fernandez <contact@pablofernandez.com>
2016-12-16 19:39:07 -0800

    Update score in database

--- update.py
+++ update.py
@@ -1,21 +1,6 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
-"""
-******************************************************************************
-Intellecutal Property Notice:
-
-The following confidential program contains algorithms written by Pablo Fernandez
-that may eventually be sold or used in a commerical setting. 
-
-Please do not share or distribute this program. Copyright 2016. 
-
-Thank you.
-Pablo Fernandez
-www.pablofernandez.com
-******************************************************************************
-"""
-
 def update_analysis(connection, unique_entry, score):
     cursor = connection.cursor()
     sql = "UPDATE StockTweets SET Tweet_Sentiment='%s' WHERE Unique_Entry='%s'"
